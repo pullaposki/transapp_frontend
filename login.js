26e385f1c4fb32b8b14fe0ef4c18c6849054f5ea
pullaposki <pullaposki@windowslive.com>
2024-01-20 11:32:36 +0200

    Initial commit

---
+++ login.js
@@ -0,0 +1,32 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    fetch("http://localhost/03_trans_app_backend/login.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          // Login was successful
+          console.log("Logged in!");
+        } else {
+          // Login failed
+          console.log("Login failed. ", data.status, " ", data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
