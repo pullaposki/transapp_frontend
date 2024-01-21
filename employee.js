fedb9f012e8bef909a9bc3c357a3b1117e66819a
pullaposki <pullaposki@windowslive.com>
2024-01-21 13:01:09 +0200

    Add employee management functionality

--- employee.js
+++ employee.js
@@ -15,8 +15,67 @@ function createEmployeeListItem(employee) {
 
   const manageButton = document.createElement("button");
   manageButton.textContent = "Manage";
-  manageButton.addEventListener("click", function () {
+
+  // Get employee by id
+  manageButton.addEventListener("click", async function () {
     console.log(`Manage button clicked for employee ${employee.id}`);
+    const data = await fetchData(
+      `http://localhost/_projects/03_transapp_backend/get_employee_by_id.php?id=${employee.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (data.status === "success") {
+      console.log(data.message);
+      messageArea.textContent = JSON.stringify(data.message);
+
+      // Change manage button to close button
+      manageButton.textContent = "Close";
+
+      // Create manage area
+      const manageArea = document.createElement("div");
+
+      const br1 = document.createElement("br");
+      manageArea.appendChild(br1);
+
+      // Create input field for changing the name
+      const nameInput = document.createElement("input");
+      nameInput.type = "text";
+      nameInput.value = data.message.first_name;
+      manageArea.appendChild(nameInput);
+
+      // Create update button
+      const updateButton = document.createElement("button");
+      updateButton.textContent = "update";
+      manageArea.appendChild(updateButton);
+      const br2 = document.createElement("br");
+      manageArea.appendChild(br2);
+
+      const br3 = document.createElement("br");
+      manageArea.appendChild(br3);
+
+      // Create delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      manageArea.appendChild(deleteButton);
+
+      const hr = document.createElement("hr");
+      manageArea.appendChild(hr);
+
+      // Append manage area to list item
+      listItem.appendChild(manageArea);
+    } else {
+      console.log(
+        "Employee id get unsuccessful. ",
+        data.status,
+        " ",
+        data.message
+      );
+    }
   });
 
   listItem.appendChild(manageButton);
@@ -25,15 +84,13 @@ function createEmployeeListItem(employee) {
 }
 
 // Fetch employees and update the UI
-fetchData("http://localhost/03_trans_app_backend/get_employees.php", {
+fetchData("http://localhost/_projects/03_transapp_backend/get_employees.php", {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
   },
 })
   .then((employees) => {
-    console.log(employees);
-
     const employeeListElement = document.getElementById("employeeList");
     employees.forEach((employee) => {
       const listItem = createEmployeeListItem(employee);
@@ -57,7 +114,7 @@ document
     messageArea.textContent = "Adding employee...";
 
     const data = await fetchData(
-      "http://localhost/03_trans_app_backend/add_employee.php",
+      "http://localhost/_projects/03_transapp_backend/add_employee.php",
       {
         method: "POST",
         headers: {
